f9e187f478a97e7e31e37cff6b4d441ce32a325d
sam_trg <samarthasija7@gmail.com>
2024-04-13 00:57:32 +0530

    theme-gui

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from tkinter import *
 import tkinter.messagebox
 from tktimepicker import AnalogPicker, AnalogThemes, constants
 from PIL import Image, ImageTk
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+ctk.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
 
 def generate_pnr():
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
@@ -29,13 +35,13 @@ def generate_qrcode(text):
 
 def display_qrcode():
 
-    qr_window = Toplevel(root)
+    qr_window = ctk.CTkToplevel(app)
     qr_window.iconphoto(False, PhotoImage(file='icon.png'))
     qr_window.title("QR Code")
     image_path = "boarding_pass_qr.png"
     image = image = Image.open(image_path)
     photo = ImageTk.PhotoImage(image)
-    image_label = Label(root, image=photo)
+    image_label = ctk.CTkLabel(app, image=photo)
     image_label.grid(row=0, column=0)
 
 
@@ -49,7 +55,7 @@ def updateTime(time):
     
 def get_time():
 
-    top = Toplevel(root)
+    top = ctk.CTkToplevel(app)
     top.iconphoto(False, PhotoImage(file='clock_icon.png'))
     top.title("Time Picker")
 
@@ -57,7 +63,7 @@ def get_time():
     time_picker.pack(expand=True, fill="both")
 
     theme = AnalogThemes(time_picker)
-    ok_btn = Button(top, text="Okay", command=lambda: updateTime(time_picker.time()))
+    ok_btn = ctk.CTkButton(top, text="Okay", command=lambda: updateTime(time_picker.time()))
     ok_btn.pack()
 
 def check_in(conn, c, entries):
@@ -96,7 +102,7 @@ def openNewWindow(data: tuple):
 
     # Toplevel object which will 
     # be treated as a new window
-    newWindow = Toplevel(root)
+    newWindow = ctk.CTkToplevel(app)
    
     newWindow.iconphoto(False, PhotoImage(file='icon.png'))
     # sets the title of the
@@ -104,49 +110,49 @@ def openNewWindow(data: tuple):
     newWindow.title("Pass")
  
     # sets the geometry of toplevel
-    newWindow.geometry("220x240")
+    newWindow.geometry("280x290")
     
-    # A Label widget to show in toplevel
-    Label(newWindow, text="Name: ").grid(row=0, column=0)
-    Label(newWindow, text="PNR: ").grid(row=1, column=0)
-    Label(newWindow, text="Airline: ").grid(row=2, column=0)
-    Label(newWindow, text="Flight number: ").grid(row=3, column=0)
-    Label(newWindow, text="").grid(row=4, column=0)
-    Label(newWindow, text="Boarding Time: ").grid(row=5, column=0)
-    Label(newWindow, text="Departure Airport: ").grid(row=6, column=0)
-    Label(newWindow, text="Arrival Airport: ").grid(row=7, column=0)
-    Label(newWindow, text="").grid(row=8, column=0)
+    # A ctk.CTkLabel widget to show in toplevel
+    ctk.CTkLabel(newWindow, text="Name: ").grid(row=0, column=0)
+    ctk.CTkLabel(newWindow, text="PNR: ").grid(row=1, column=0)
+    ctk.CTkLabel(newWindow, text="Airline: ").grid(row=2, column=0)
+    ctk.CTkLabel(newWindow, text="Flight number: ").grid(row=3, column=0)
+    ctk.CTkLabel(newWindow, text="").grid(row=4, column=0)
+    ctk.CTkLabel(newWindow, text="Boarding Time: ").grid(row=5, column=0)
+    ctk.CTkLabel(newWindow, text="Departure Airport: ").grid(row=6, column=0)
+    ctk.CTkLabel(newWindow, text="Arrival Airport: ").grid(row=7, column=0)
+    ctk.CTkLabel(newWindow, text="").grid(row=8, column=0)
 
 
     text=f'''Passenger Mr./Ms. {data[0]} with PNR number {data[7]} travelling via {data[2]} flight number {data[3]} departing from {data[4]} airport to {data[5]} airport at {data[6]} hrs.
     \nPlease note that boarding begins at {data[6]-100} hrs and closes 20 minutes before departure.\nHave a safe journey!'''
 
-    Button(newWindow, text="  Generate QR  ", command=lambda: generate_qrcode(text)).grid(row=9, column=0)
-    Button(newWindow, text="     Show QR     ", command=lambda: Image.open('boarding_pass_qr.png').show()).grid(row=9, column=1)
+    ctk.CTkButton(newWindow, text="Generate QR", command=lambda: generate_qrcode(text)).grid(row=9, column=0)
+    ctk.CTkButton(newWindow, text="  Show QR  ", command=lambda: Image.open('boarding_pass_qr.png').show()).grid(row=9, column=1)
  
 
 
 
     '''
     #Creating gap
-    Label(newWindow, text="\t").grid(row=0, column=1)
-    Label(newWindow, text="\t").grid(row=1, column=1)
-    Label(newWindow, text="\t").grid(row=2, column=1)
-    Label(newWindow, text="\t").grid(row=3, column=1)
-    Label(newWindow, text="\t").grid(row=4, column=1)
-    Label(newWindow, text="\t").grid(row=5, column=1)
-    Label(newWindow, text="\t").grid(row=6, column=1)
-    Label(newWindow, text="\t").grid(row=7, column=1)
+    ctk.CTkLabel(newWindow, text="\t").grid(row=0, column=1)
+    ctk.CTkLabel(newWindow, text="\t").grid(row=1, column=1)
+    ctk.CTkLabel(newWindow, text="\t").grid(row=2, column=1)
+    ctk.CTkLabel(newWindow, text="\t").grid(row=3, column=1)
+    ctk.CTkLabel(newWindow, text="\t").grid(row=4, column=1)
+    ctk.CTkLabel(newWindow, text="\t").grid(row=5, column=1)
+    ctk.CTkLabel(newWindow, text="\t").grid(row=6, column=1)
+    ctk.CTkLabel(newWindow, text="\t").grid(row=7, column=1)
     '''
-     # A Label widget to show in toplevel
-    Label(newWindow, text=f"{data[0]}").grid(row=0, column=1)
-    Label(newWindow, text=f"{data[7]}").grid(row=1, column=1)
-    Label(newWindow, text=f"{data[2]}").grid(row=2, column=1)
-    Label(newWindow, text=f"{data[3]}").grid(row=3, column=1)
-    Label(newWindow, text="").grid(row=4, column=1)
-    Label(newWindow, text=f"{data[6]-100} hrs").grid(row=5, column=1)
-    Label(newWindow, text=f"{data[4]}").grid(row=6, column=1)
-    Label(newWindow, text=f"{data[5]}").grid(row=7, column=1)
+     # A ctk.CTkLabel widget to show in toplevel
+    ctk.CTkLabel(newWindow, text=f"{data[0]}").grid(row=0, column=1)
+    ctk.CTkLabel(newWindow, text=f"{data[7]}").grid(row=1, column=1)
+    ctk.CTkLabel(newWindow, text=f"{data[2]}").grid(row=2, column=1)
+    ctk.CTkLabel(newWindow, text=f"{data[3]}").grid(row=3, column=1)
+    ctk.CTkLabel(newWindow, text="").grid(row=4, column=1)
+    ctk.CTkLabel(newWindow, text=f"{data[6]-100} hrs").grid(row=5, column=1)
+    ctk.CTkLabel(newWindow, text=f"{data[4]}").grid(row=6, column=1)
+    ctk.CTkLabel(newWindow, text=f"{data[5]}").grid(row=7, column=1)
 
 
 
@@ -169,37 +175,37 @@ if __name__ == "__main__":
     conn, c = connect_db()
     create_table(conn, c)  # Create table if it doesn't exist
 
-    root = Tk()
-    root.title("Boarding Pass Generator")
-    root.iconphoto(False, PhotoImage(file='icon.png'))
+    app = ctk.CTk()
+    app.title("Boarding Pass Generator")
+    app.iconphoto(False, PhotoImage(file='icon.png'))
 
-    Label(root, text="Enter your name: ").grid(row=0)
-    Label(root, text="Enter phone number: ").grid(row=1)
-    Label(root, text="Enter airline: ").grid(row=2)
-    Label(root, text="Enter flight number: ").grid(row=3)
-    Label(root, text=" Enter departure airport (e.g., Bangalore (BLR)): ").grid(row=4)
-    Label(root, text=" Enter arrival airport (e.g., Delhi (DEL)): ").grid(row=5)
-    Label(root, text="Choose flight departure time: ").grid(row=6)
+    ctk.CTkLabel(app, text="Enter your name: ").grid(row=0)
+    ctk.CTkLabel(app, text="Enter phone number: ").grid(row=1)
+    ctk.CTkLabel(app, text="Enter airline: ").grid(row=2)
+    ctk.CTkLabel(app, text="Enter flight number: ").grid(row=3)
+    ctk.CTkLabel(app, text=" Enter departure airport (e.g., Bangalore (BLR)): ").grid(row=4)
+    ctk.CTkLabel(app, text=" Enter arrival airport (e.g., Delhi (DEL)): ").grid(row=5)
+    ctk.CTkLabel(app, text="Choose flight departure time: ").grid(row=6)
    
 
-    entries = [Entry(root) for _ in range(7)]
+    entries = [ctk.CTkEntry(app) for _ in range(7)]
     for i, entry in enumerate(entries):
         entry.grid(row=i, column=1)
     
     
     time = ()
-    time_lbl = Label(root)
-    time_btn = Button(root, text="Get Time", command=get_time)
+    time_lbl = ctk.CTkLabel(app)
+    time_btn = ctk.CTkButton(app, text="Get Time", command=get_time)
     time_lbl.grid(row=7, column=1)
     time_btn.grid(row=7, column=0)
     
 
-    Button(root, text='  Click to Check In  ', command=lambda: check_in(conn, c, entries)).grid(row=8, column=1, sticky=W, pady=4)
+    ctk.CTkButton(app, text='  Click to Check In  ', command=lambda: check_in(conn, c, entries)).grid(row=8, column=1, sticky=W, pady=4)
 
-    Label(root, text="Enter your PNR: ").grid(row=9)
-    pnr_entry = Entry(root)
+    ctk.CTkLabel(app, text="Enter your PNR: ").grid(row=9)
+    pnr_entry = ctk.CTkEntry(app)
     pnr_entry.grid(row=9, column=1)
-    Button(root, text='Print Boarding Pass', command=lambda: print_boarding_pass(conn, c, pnr_entry)).grid(row=10, column=1, sticky=W, pady=4)
+    ctk.CTkButton(app, text='Print Boarding Pass', command=lambda: print_boarding_pass(conn, c, pnr_entry)).grid(row=10, column=1, sticky=W, pady=4)
 
 
-    mainloop()
+    app.mainloop()
